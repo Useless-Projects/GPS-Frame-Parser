609b5935fc5d002a95c57f1b2fdee8be987413f7
Thibault Savenkoff <66521530+Thibault-Savenkoff@users.noreply.github.com>
2023-03-10 10:58:37 +0100

    GPS Frame Parser
    I add a About Menu, and remove Milliseconds if they're not present.

--- GPS Frame Parser.py
+++ GPS Frame Parser.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import tkinter.messagebox as messagebox
 
 def parse_gps_frame():
     text = gps_input.get()
@@ -9,13 +10,26 @@ def parse_gps_frame():
         number = field[8]
         number_numerique = float(number)
         number_arrondi = round(number_numerique)
-
-        result_text.set("Time: " + time[1:3] + "h" + " " + time[3:5] + "m" + " " + time[5:6] + "s" + " " + time[8:11] + "ms\n"
+        if time[8:11] == "":
+            result_text_widget.configure(state="normal")
+            result_text_widget.delete("1.0", "end")
+            result_text_widget.insert("1.0", "Time: " + time[1:3] + "h" + " " + time[3:5] + "m" + " " + time[5:6] + "s\n"
+                            "Latitude: " + (field[2]) + " " + (field[3]) + "\n"
+                            "Longitude: " + (field[4]) + " " + (field[5]) + "\n"
+                            "Altitude: " + (field[9]) + (field[10]) + "\n"
+                            "Satellites:" + (field[7]) + "\n"
+                            "Reliability: " + get_reliability_string(number_arrondi), "center")
+            result_text_widget.configure(state="disabled")
+        else:
+            result_text_widget.configure(state="normal")
+            result_text_widget.delete("1.0", "end")
+            result_text_widget.insert("1.0", "Time: " + time[1:3] + "h" + " " + time[3:5] + "m" + " " + time[5:6] + "s" + " " + time[8:11] + "ms\n"
                          "Latitude: " + (field[2]) + " " + (field[3]) + "\n"
                          "Longitude: " + (field[4]) + " " + (field[5]) + "\n"
                          "Altitude: " + (field[9]) + (field[10]) + "\n"
                          "Satellites:" + (field[7]) + "\n"
-                         "Reliability: " + get_reliability_string(number_arrondi))
+                         "Reliability: " + get_reliability_string(number_arrondi), "center")
+            result_text_widget.configure(state="disabled")
     else:
         result_text.set("This is not a GPS Frame!")
 
@@ -37,6 +51,15 @@ def get_reliability_string(number):
 window = tk.Tk()
 window.title("GPS Frame Parser")
 
+# Create the menu bar
+menu_bar = tk.Menu(window)
+
+# Add the "About" command to the menu bar
+menu_bar.add_command(label="About", command=lambda: messagebox.showinfo("About", "GPS Frame Parser v1.1.0\nCoded by Thibault Savenkoff\nLicensed under the GNU General Public License v3.0.\n© 2023 Thibault Savenkoff. All rights reserved."))
+
+# Set the menu bar for the window
+window.config(menu=menu_bar)
+
 # Create an input field to enter the GPS frame
 gps_input = tk.Entry(window, width=50)
 gps_input.pack(pady=10)
@@ -47,8 +70,17 @@ submit_button.pack(pady=10)
 
 # Create a text field to display the result
 result_text = tk.StringVar()
-result_label = tk.Label(window, textvariable=result_text, wraplength=500)
-result_label.pack(pady=10)
+result_text_widget = tk.Text(window, wrap="word", height=8, width=50, bd=0, highlightthickness=0)
+result_text_widget.insert("1.0", result_text.get())
+result_text_widget.configure(state="disabled")
+result_text_widget.pack(pady=10)
+result_text_widget.tag_configure("center", justify="center")
+
+def copy_to_clipboard(event):
+    window.clipboard_clear()
+    window.clipboard_append(result_text_widget.get("1.0", "end"))
+
+result_text_widget.bind("<Button-1>", copy_to_clipboard)
 
 # Start the main loop of the window
 window.mainloop()
